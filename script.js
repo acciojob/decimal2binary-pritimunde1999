438555de20908f683d06d4f6d9a671f98c02249d
Priti Munde <120581202+pritimunde1999@users.noreply.github.com>
2023-07-21 15:18:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 function decimalToBinary(num) {
   //Write you code here
-  
+	let ans ="";
+  while(num>0)
+	  {
+		  let digit = num%2;
+		  ans += digit;
+		  num = num/2;
+	  }
+
+	let ans1="";
+	for(let i=ans.length-1; i>=0;i--)
+	{
+		ans1+= ans.charAt(i);
+	}
+
+	return Number(ans1);
 }
 
 window.decimalToBinary = decimalToBinary;
